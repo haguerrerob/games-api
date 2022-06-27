6e8b5dfdeff254eb8b052cc91d70ec8f682c082f
haguerrerob <haguerrerob@gmail.com>
2022-06-27 18:51:25 -0300

    Changes in db.js

--- src/db.js
+++ src/db.js
@@ -13,7 +13,13 @@ const sequelize = new Sequelize(
   //     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/gamesapi`,
   {
     logging: false, // set to console.log to see the raw SQL queries
-    native: false // lets Sequelize know we can use pg-native for ~30% more speed
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 )
 const basename = path.basename(__filename)
