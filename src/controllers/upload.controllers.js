7affe012c825279b7a329edaa41b963ddbdb28e5
haguerrerob <haguerrerob@gmail.com>
2022-06-29 18:43:13 -0300

    public/img

--- src/controllers/upload.controllers.js
+++ src/controllers/upload.controllers.js
@@ -1,22 +1,23 @@
-const fs = require('fs');       //File System - for file manipulation
-
+const fs = require('fs') //File System - for file manipulation
 
 export const uploadThumbnailBW = async (req, res) => {
-    try {
-        let dir = __dirname.replace("\\controllers", "")
-        fs.stat(dir + '/img/' + req.params.thumbnail_half, function (err, stat) {
-            if (err == null) {
-                console.log('File exists');
-                res.sendFile(dir + '/img/' + req.params.thumbnail_half)
-            } else if (err.code === 'ENOENT') {
-                console.log('File not exists');
-                res.status(404)
-            } else {
-                console.log('Some other error: ', err.code);
-                res.status(500)
-            }
-        })
-    } catch (err) {
-        console.error(err);
-    }
-};
+  try {
+    let dir = __dirname.replace('\\controllers', '')
+    // fs.stat(dir + '/img/' + req.params.thumbnail_half, function (err, stat) {
+    fs.stat('img/' + req.params.thumbnail_half, function (err, stat) {
+      if (err == null) {
+        console.log('File exists')
+        // res.sendFile(dir + '/img/' + req.params.thumbnail_half)
+        res.sendFile('img/' + req.params.thumbnail_half)
+      } else if (err.code === 'ENOENT') {
+        console.log('File not exists')
+        res.status(404)
+      } else {
+        console.log('Some other error: ', err.code)
+        res.status(500)
+      }
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
