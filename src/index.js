16abdbf194ceabb60386f92d0f81f01e9aa67c77
haguerrerob <haguerrerob@gmail.com>
2022-07-05 10:31:31 -0300

    force true to false sequelize connect

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { conn } = recursos
 const PORT = process.env.PORT || 4000
 
 // Sync all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`) // eslint-disable-line no-console
   })
